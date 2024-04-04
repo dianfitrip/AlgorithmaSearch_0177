e26bfd2bbc32ad53b636ca73ec0d6ceeb44b602a
dianfitrip <dianfitri683@gmail.com>
2024-04-04 11:06:07 +0700

    prosedur linearsearch part4

--- Searching_linierbiner/Searching_linierbiner.cpp
+++ Searching_linierbiner/Searching_linierbiner.cpp
@@ -53,7 +53,15 @@ void LinearSearch()
 				cout << "\n" << item << " found at position " << (i + 1) << endl;
 				break;
 			}
+			i++;
 		}
-	}
+
+		if (i == n)
+			cout << "\n" << item << "not found in the array\n";
+		cout << "\nNumber of comparisons: " << ctr << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'y'));
 }
 
