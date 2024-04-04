988038c5111467d8e8bf320bd3500f56deb347ba
dianfitrip <dianfitri683@gmail.com>
2024-04-04 11:00:03 +0700

    prosedur linearsearch part3. bagian ....while

--- Searching_linierbiner/Searching_linierbiner.cpp
+++ Searching_linierbiner/Searching_linierbiner.cpp
@@ -45,6 +45,15 @@ void LinearSearch()
 
 		ctr = 0;
 		i = 0;
+		while (i < n)
+		{
+			ctr++;
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl;
+				break;
+			}
+		}
 	}
 }
 
